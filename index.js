86f17d22202d58309e78439a1f9ca57fbae63c96
mrtianjin829 <100748047+mrtianjin829@users.noreply.github.com>
2023-08-03 10:22:13 +0530

    Prettify Code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-console.log("WaveBin (on branch main)")
+console.log("WaveBin (on branch main)");
 const { WaveFile } = require("wavefile");
 const fs = require("fs");
 const file = process.argv[2];
@@ -8,16 +8,14 @@ const wf = new WaveFile();
 wf.fromBuffer(fd);
 wf.toSampleRate(32000);
 wf.toBitDepth("8");
-let smp = wf.getSamples()
+let smp = wf.getSamples();
 let n;
 // properly interleave the channels
-if(wf.fmt.numChannels==2){
-let [a,b]=smp
-n = a.map((v,i)=>((v+b[i])/2))
+if (wf.fmt.numChannels == 2) {
+  let [a, b] = smp;
+  n = a.map((v, i) => (v + b[i]) / 2);
 } else {
-  n = smp
+  n = smp;
 }
-n=new Uint8Array(n)
-fs.writeFileSync("out.bin",Buffer.from(n))
-
-
+n = new Uint8Array(n);
+fs.writeFileSync("out.bin", Buffer.from(n));
