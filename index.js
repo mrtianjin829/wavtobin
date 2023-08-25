f4ff0df497d6524d73d74a46d9024ec3252e2f9f
mrtianjin829 <100748047+mrtianjin829@users.noreply.github.com>
2023-08-25 21:43:00 +0530

    Addcomments to document entry point

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 #!/usr/bin/node
 console.log("WaveBin");
 try {
+  // Import library for handling wav files.
   const { WaveFile } = require("wavefile");
+  // Load filesystem API
   const fs = require("fs");
+  // Initialize output file as an wave file
   const wf = new WaveFile();
+  // Procure argyments
   const cf = require("./argp")(process.argv)
+  // Read the file.
   const fd = fs.readFileSync(cf.file);
+  // Load input file into wf.
   wf.fromBuffer(fd);
+  /*do important conversions
+   *this will convert the sample-type into PCM u8
+   *changes the sample rate to the desired rate otherwise 32 Khz (good for html5bytebeat)
+   * */
   wf.toSampleRate(cf.sampleRate);
   wf.toBitDepth("8");
+  // Obtain "samples" from audio
   let smp = wf.getSamples();
-  let n;
+  let n; // contains the PCM buffer for output file
   // properly interleave the channels
   if (wf.fmt.numChannels == 2) {
     let [a, b] = smp;
+    // Use average to mix stereo channels.
     n = a.map((v, i) => (v + b[i]) / 2);
   } else {
     n = smp;
   }
   n = new Uint8Array(n);
-  fs.writeFileSync(cf.output, Buffer.from(n));
+  fs.writeFileSync(cf.output, Buffer.from(n)); // Write output.
 } catch (err) {
+  // Definitely helpful error handling.
   console.log("[wtb] an error occurred:");
   throw err
   process.exit(1);
