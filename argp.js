039efc72006cad3a83c8ce2b66bdad846cff9b58
mrtianjin829 <100748047+mrtianjin829@users.noreply.github.com>
2023-09-23 23:23:28 +0530

    FFmpeg custom arguments

--- argp.js
+++ argp.js
@@ -4,6 +4,7 @@ function gen(args) {
      sampleRate: 32000,
      output: "out.bin",
      useFFmpeg: false,
+     ffmpegArg: ["",""]
      help: false
   };
   args.forEach((v,i)=>{
@@ -17,6 +18,11 @@ function gen(args) {
         cf.output = args[i+1]
      } else if(v == "--ffmpeg"){
         cf.useFFmpeg = true
+       else if(v == "-fi"){
+        cf.ffmpegArg[0] = args[i+1]
+       } else if(v == "-fo"){
+        cf.ffmpegArg[1] = args[i+2]
+       }
      } else {
         cf.file = v
      }
