3bf6516294b0cd3ca65b82dd7da0bc2525cbc296
mrtianjin829 <100748047+mrtianjin829@users.noreply.github.com>
2023-09-24 15:27:40 +0530

    FFmpeg arguments for two streams

--- argp.js
+++ argp.js
@@ -1,26 +1,41 @@
 function gen(args) {
   let cf = {
-     file: "",
-     sampleRate: 32000,
-     output: "out.bin",
-     useFFmpeg: false,
-     help: false
+    file: "",
+    sampleRate: 32000,
+    output: "out.bin",
+    useFFmpeg: false,
+    ffmpegArg: ["", ""],
+    help: false,
   };
-  args.forEach((v,i)=>{
-     if(i <= 1) return
-     
-     if(v == "--sampleRate" || v == "-r"){
-        cf.sampleRate = parseInt(args[i+1]);
-     }  else if(v == "--help" || v == "-h") {
-	cf.help = true
-     }  else if(v == "--output" || v == "-o"){
-        cf.output = args[i+1]
-     } else if(v == "--ffmpeg"){
-        cf.useFFmpeg = true
-     } else {
-        cf.file = v
-     }
-  })
+  let parami = []
+   function check(i){
+       for(let j = 0; j < parami.length; j++){
+	   if(parami[j] == i) return true;
+       };
+   }
+  args.forEach((v, i) => {
+    if (i <= 1) return;
+    if (check(i)) return;
+    if (v == "--sampleRate" || v == "-r") {
+      cf.sampleRate = parseInt(args[i + 1]);
+      parami.push(i+1)
+    } else if (v == "--help" || v == "-h") {
+      cf.help = true;
+    } else if (v == "--output" || v == "-o") {
+      cf.output = args[i + 1];
+      parami.push(i+1)
+    } else if (v == "--ffmpeg") {
+      cf.useFFmpeg = true;
+    } else if (v == "-fi") {
+      cf.ffmpegArg[0] = args[i + 1];
+      parami.push(i+1)
+    } else if (v == "-fo") {
+      cf.ffmpegArg[1] = args[i + 1];
+      parami.push(i+1)
+    } else {
+      cf.file = v;
+    }
+  });
   return cf;
 }
 
